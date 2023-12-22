5964480ec09dfbad9c07bd5823d5784affae3d3d
Suhel Khan <112371384+SuhelKhanCA@users.noreply.github.com>
2023-12-22 13:52:25 +0000

    Add from codespace

---
+++ js/01variable.js
@@ -0,0 +1,18 @@
+const accountID = 1224
+let accountEmail = "hit@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+//accountID = 2
+
+/*
+
+preffered not to use 'var'
+
+*/
+
+console.log(accountID)
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
+
