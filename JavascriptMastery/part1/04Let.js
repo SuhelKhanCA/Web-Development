3f4862e620002bb5617009432dd46b14a51195b0
suhelkhanca <suhel.hata@gmail.com>
2024-04-08 16:21:48 +0530

    let keywords

--- JavascriptMastery/part1/04Let.js
+++ JavascriptMastery/part1/04Let.js
@@ -4,6 +4,13 @@
 let firstName = "harshit";
 firstName = "Mohit";
 console.log(firstName);
-
-
+// Note:
+/*
+var myVar = 5;
+var myVar = 2; // declaring multiple time for same name allowed via 'var' but stricted with 'let'
+*/
+var myVar = 5;
+console.log(myVar);
+var myVar = 2;
+console.log(myVar);
 // block scope vs funtion scope (covered later in this video)
