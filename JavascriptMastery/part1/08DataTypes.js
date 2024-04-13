e38d738aa95031a4cbfb6fc1a4657a5a9613c778
suhelkhanca <suhel.hata@gmail.com>
2024-04-13 11:34:52 +0530

    data types and typeof operator

---
+++ JavascriptMastery/part1/08DataTypes.js
@@ -0,0 +1,30 @@
+// typeof operator 
+
+// data types (primitive data types)
+// 1) string "harhit"
+// 2) number 2, 4, 5.6 
+// 3) booleans 
+// 4) undefined
+// 5) null 
+// 6) BigInt
+// 7) Symbol
+
+let age = 22; 
+let firstName = "harshit";
+
+console.log(typeof age); // => number
+
+// 22 -> "22"
+// convert number to string. 
+age = age + "";
+console.log(typeof(age));  // "22"
+
+
+// convert string to number. 
+
+let myStr = +"34"; // trick
+console.log(typeof myStr);
+
+let age2 = "18";
+age2 = Number(age2);
+console.log(typeof age2);
