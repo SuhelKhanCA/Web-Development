14982f6a6a8fb4da6a6b4c59455f69c356b1f8bd
suhelkhanca <suhel.hata@gmail.com>
2024-07-31 21:53:53 +0530

    ellipsis or rest param

--- JavascriptMastery/part1/52.js
+++ JavascriptMastery/part1/52.js
@@ -8,7 +8,7 @@
 
 // myFunc(3,4,5,6,7,8,9);
 
-function addAll(...numbers){
+function addAll(...numbers){ // ellipsis or rest param
     let total = 0;
     for(let number of numbers){
         total = total + number;
