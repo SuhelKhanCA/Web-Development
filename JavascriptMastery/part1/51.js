182c9c19cc12977c6ce961e7114faad614a1bb79
suhelkhanca <suhel.hata@gmail.com>
2024-07-31 21:51:35 +0530

    default paramter

--- JavascriptMastery/part1/51.js
+++ JavascriptMastery/part1/51.js
@@ -7,9 +7,10 @@
 //     return a+b;
 // }
 
-function addTwo(a,b=0){
+function addTwo(a,b=0){ // default parameter b=0
     return a+b;
 }
 
+console.log(addTwo(3))
 const ans = addTwo(4, 8);
 console.log(ans);
