ca08eb2ffaf1b088b41baf8052bbb0ae737e776c
suhelkhanca <suhel.hata@gmail.com>
2024-07-31 21:44:16 +0530

    solve DB connection error

--- flaskDb/app.py
+++ flaskDb/app.py
@@ -2,7 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
