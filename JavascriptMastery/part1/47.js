e50245920fcd30348371cb0f51fde2b46164e20f
suhelkhanca <suhel.hata@gmail.com>
2024-07-31 21:21:03 +0530

    Hoisting intro

--- JavascriptMastery/part1/47.js
+++ JavascriptMastery/part1/47.js
@@ -1,10 +1,18 @@
 // hoisting 
 
-// hello();
+hello(); // ok
 
-// function hello(){
-//     console.log("hello world");
-// }
-// console.log(hello);
+function hello(){
+    console.log("hello world");
+}
+
+// hi(); // Error: Cannot access 'hi' before initialization
+let hi = function(){
+    console.log("Hi there");
+}
+hi();
 // const hello = "hello world";
 // console.log(hello);
+
+console.log(hell);
+var hell = "Hello World"
