67df97792f7464641febbec0c73f243063edcfe4
suhelkhanca <suhel.hata@gmail.com>
2024-07-31 20:15:02 +0530

    Iterating array objects

--- JavascriptMastery/part1/38.js
+++ JavascriptMastery/part1/38.js
@@ -6,17 +6,18 @@ const person = {
 }
 
 // for in loop 
-// Object.keys 
+ 
 
 // for(let key in person){
-//     // console.log(`${key} : ${person[key]}`);
-//     console.log(key," : " ,person[key]);
+//     console.log(`${key} : ${person[key]}`);
+//     // console.log(key," : " ,person[key]);
 // }
 
 // console.log(typeof (Object.keys(person)));
 // const val = Array.isArray((Object.keys(person)));
 // console.log(val);
 
-// for(let key of Object.keys(person)){
-//     console.log(person[key]);
-// }
+// Object.keys
+for(let key of Object.keys(person)){
+    console.log(person[key]);
+}
