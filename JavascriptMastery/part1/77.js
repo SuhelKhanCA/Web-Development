32c30540dab502d8a9dc9da6ac8ae078293f7dab
suhelkhanca <suhel.hata@gmail.com>
2024-08-02 19:49:29 +0530

    Creating objects

--- JavascriptMastery/part1/77.js
+++ JavascriptMastery/part1/77.js
@@ -15,11 +15,12 @@ function createUser(firstName, lastName, email, age, address){
     user.is18 =  function(){
         return this.age >= 18;
     }
-    return user;
+    return user; // returning 'user'
 }
 
 const user1 = createUser('harshit', 'vashsith', 'harshit@gmail.com', 19, "my address");
 console.log(user1);
 const is18 = user1.is18();
+console.log(is18);
 const about = user1.about();
 console.log(about);
