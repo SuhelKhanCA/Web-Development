89d16bf80e7cb65fb5489b2790674464c00979e3
suhelkhanca <suhel.hata@gmail.com>
2024-08-02 19:39:22 +0530

    arrow fxn and one level upper object

--- JavascriptMastery/part1/75.js
+++ JavascriptMastery/part1/75.js
@@ -3,9 +3,10 @@
 const user1 = {
     firstName : "harshit",
     age: 8,
-    about: () => {
+    about: () => { // arrow 'fxn' this surrounding se leta h
         console.log(this.firstName, this.age);
     }   
 }
 
-user1.about(user1);
+// user1.about(); XX
+// user1.about(user1); XX
